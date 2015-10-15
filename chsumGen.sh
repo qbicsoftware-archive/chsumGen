6700b847fa095405d9755fd4474013fb342088f3
Sven Fillinger <sven.fillinger@mailbox.org>
2015-10-15 14:05:37 +0200

    added checksumGen shell script

---
+++ chsumGen.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+########### Checksum Generator
+#
+# Reads the files of a directory and calcs the checksums
+#
+
+# The directory with the files to create the checksums from
+DIR_FILES="$1"
+
+#  Check if command param is provided or is == '-h'
+if [ -z "$1" -o "$1" == "-h" ]; then
+	printf '%s' "Usage:
+${PROGNAME} [DIRECTORY_WITH_FILES]"
+	exit 1
+fi
+
+# Write the checksum for every file in the directory
+if [ -d  "$DIR_FILES" ]; then
+  for file in "$DIR_FILES"/*
+    do
+      if [ -f "$file" ]; then
+        echo "$file"
+        sha256sum "$file" > "${file%.*}.sha256_checksum"
+      fi
+    done
+fi
